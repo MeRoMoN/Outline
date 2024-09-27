ca3250f4c1eb50bce21bda64516a1bf6b0b6c1ce
MeRoMoN <owner@meromon.com>
2024-09-27 20:47:25 +1000

    Implemented server retrieval and joining in 'Find Game' menu

--- Source/Outline/Private/Game/OutlineGameInstanceBase.cpp
+++ Source/Outline/Private/Game/OutlineGameInstanceBase.cpp
@@ -2,15 +2,72 @@
 
 
 #include "Outline/Public/Game/OutlineGameInstanceBase.h"
+#include "JsonObjectConverter.h"
 
 
 UOutlineGameInstanceBase::UOutlineGameInstanceBase()
 {
-
+	Http = &FHttpModule::Get();
 }
 
 void UOutlineGameInstanceBase::GoToMap(FString MapURL)
 {
 	GetWorld()->ServerTravel(MapURL);
+}
+
+void UOutlineGameInstanceBase::RetrieveServerList()
+{
+	TSharedRef<IHttpRequest> Request = Http->CreateRequest();
+
+	Request->OnProcessRequestComplete().BindUObject(this, &UOutlineGameInstanceBase::OnServerListProcessRequestComplete);
+
+	Request->SetURL("https://localhost:44349/api/Host");
+	Request->SetVerb("GET");
+	Request->SetHeader(TEXT("Content-Type"), TEXT("application/json"));
+
+	Request->ProcessRequest();
+}
+
+TArray<FServerData>& UOutlineGameInstanceBase::GetServerList()
+{
+	return ServerList;
+}
+
+void UOutlineGameInstanceBase::OnServerListProcessRequestComplete(FHttpRequestPtr Request, FHttpResponsePtr Response, bool Success)
+{
+	ServerList.Empty();
+	if (Success)
+	{
+		FString ResponseStr = Response->GetContentAsString();
+		ResponseStr.InsertAt(0, FString("{\"Response\":"));
+		ResponseStr.AppendChar('}');
+		UE_LOG(LogTemp, Warning, TEXT("Response: %s"), *ResponseStr);
+
+		TSharedPtr<FJsonObject> JsonObject = MakeShareable(new FJsonObject);
+		TSharedRef<TJsonReader<>> JsonReader = TJsonReaderFactory<TCHAR>::Create(ResponseStr);
+
+		if (FJsonSerializer::Deserialize(JsonReader, JsonObject) && JsonObject.IsValid())
+		{
+			TArray<TSharedPtr<FJsonValue>> JsonValues = JsonObject->GetArrayField(TEXT("Response"));
+
+			for (TSharedPtr<FJsonValue> JsonValue : JsonValues)
+			{
+				FServerData ServerData = FServerData();
+				TSharedPtr<FJsonObject> JsonObj = JsonValue->AsObject();
+
+				if (FJsonObjectConverter::JsonObjectToUStruct(JsonObj.ToSharedRef(), &ServerData))
+				{
+					ServerList.Add(ServerData);
+				}
+			}
+
+			OnServersListReceived.Broadcast();
+		}
+
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("GetServerList Failed"));
+	}
 }
 
--- Source/Outline/Public/Game/OutlineGameInstanceBase.h
+++ Source/Outline/Public/Game/OutlineGameInstanceBase.h
@@ -1,5 +1,6 @@
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
+#include "Http.h"
 #include "OutlineGameInstanceBase.generated.h"
 
 USTRUCT(BlueprintType)
@@ -20,6 +21,31 @@ struct FMapInfo
 	class UTexture2D* MapImage;
 };
 
+USTRUCT(BlueprintType)
+struct FServerData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	int ServerID;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FString IPAddress;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FString ServerName;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FString MapName;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	int CurrentPlayers;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	int MaxPlayers;
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnServersListReceived);
 
 UCLASS()
 class OUTLINE_API UOutlineGameInstanceBase : public UGameInstance
@@ -32,6 +58,21 @@ public:
 
 protected:
 
+	UPROPERTY(BlueprintAssignable)
+	FOnServersListReceived OnServersListReceived;
+
 	UFUNCTION(BlueprintCallable)
 	void GoToMap(FString MapURL);
+
+	UFUNCTION(BlueprintCallable)
+	void RetrieveServerList();
+
+	UFUNCTION(BlueprintPure)
+	TArray<FServerData>& GetServerList();
+
+
+	FHttpModule* Http;
+	TArray<FServerData> ServerList;
+
+	void OnServerListProcessRequestComplete(FHttpRequestPtr Request, FHttpResponsePtr Response, bool Success);
 };
